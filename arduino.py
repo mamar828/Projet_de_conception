ccb59ed8dd89a5f5079a5a6937246fe32f7a5fe4
Mathieu Marquis <mamar828@ulaval.ca>
2023-11-29 16:20:17 -0500

    arduino pt1

--- arduino.py
+++ arduino.py
@@ -1,3 +1,21 @@
 import pyfirmata
+import time
 
+board = pyfirmata.Arduino('/dev/cu.usbmodem1101')
 
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+input_0 = board.get_pin("a:0:i")
+input_1 = board.get_pin("a:1:i")
+input_2 = board.get_pin("a:2:i")
+
+while True:
+    voltage_0 = input_0.read()
+    voltage_1 = input_1.read()
+    voltage_2 = input_2.read()
+     
+    if voltage:
+        print(f"{voltage*5} V")
+    else: print(None)
+    time.sleep(1)
