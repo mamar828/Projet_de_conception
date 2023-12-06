e78cd8b9b496714cad4194535f1dfd506c0f20d5
Mathieu Marquis <mamar828@ulaval.ca>
2023-12-06 18:46:33 -0500

    Working code

--- arduino.py
+++ arduino.py
@@ -6,39 +6,41 @@ board = pyfirmata.Arduino('/dev/cu.usbmodem1101')
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-# Setup pins
+# Setup name for used pins
 input_0 = board.get_pin("a:0:i")
 output = board.get_pin("d:11:p")
 
+# The following ratios are used to limit the arduino PWM output (a ratio of 1 would mean 5V, 0 would be 0V)
+max_v_ratio = 0.55
+min_v_ratio = 0.32
+
 while True:
     try:
-        # Get voltage values
-        v_1 = input_0.read()
-        v_2 = 0
-        v_0 = 9
+        # Get voltage values at different points in the circuit (v_2 and v_0 are hard-coded)
+        v_0 = 9                         # Battery voltage
+        v_1 = input_0.read()            # Between photoresistance and 1000 ohms resistance
+        v_2 = 0                         # Ground
 
-        # Check the validity of every voltage
-        if v_0 is not None:
-            v_1 *= 5        # Convert 0-1 V to 0-5 V
-            R_1 = 1000
-            R_2 = 10000
-
+        # Check the validity of the voltage read
+        if v_1 is not None:
+            v_1 *= 5            # Convert 0-1 V to 0-5 V
             current = (v_1 - v_2) / 1000
             r_photo = (v_0 - v_1) / current
-            # print(f"v_1 : {v_1:.5f}, current : {current:.5f}, r_photo : {r_photo:.5f}")
 
             # Set ratio of resistance between 1 and 0 (1 is brightest, 0 is darkest) to control voltage output
             max_ratio = 1 - (r_photo - 5000) / 15000
-            # max_ratio = 1 - (r_photo - 1000) / 199000
-            if max_ratio < 0: max_ratio = 0 
-            elif max_ratio > 1: max_ratio = 1 
-            print(f"ai_0: {v_0:.3f}, ai_1: {v_1:.3f}, ai_2: {v_2:.3f}, current: {current:.3f}, r_photo: {r_photo:.3f}, max_ratio: {max_ratio:.3f}")
+            if max_ratio < 0: max_ratio = 0
+            elif max_ratio > 1: max_ratio = 1
 
-            output.write(max_ratio)
+            print((f"ai_0: {v_0:.3f}, ai_1: {v_1:.3f}, ai_2: {v_2:.3f}, current: {current:.3f},") + 
+                   (f"r_photo: {r_photo:.3f}, max_ratio: {max_ratio:.3f}"))
+
+            output.write(max_ratio * (max_v_ratio - min_v_ratio) + min_v_ratio)
         else:
             print(f"\033[1;31;40mVoltage input missing. ai_0: {v_0}, ai_1: {v_1}, ai_2: {v_2}\033[0m")
         
     except:
         print("ERROR")
     
+    # Set updating delay to prevent CPU overloading
     time.sleep(0.1)
