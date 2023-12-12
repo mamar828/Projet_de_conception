837fd60c6413e02be72bf225a7b0614eec477959
Mathieu Marquis <mamar828@ulaval.ca>
2023-12-12 10:58:59 -0500

    final

--- arduino.py
+++ arduino.py
@@ -18,12 +18,12 @@ while True:
     try:
         # Get voltage values at different points in the circuit (v_2 and v_0 are hard-coded)
         v_0 = 9                         # Battery voltage
-        v_1 = input_0.read()            # Between photoresistance and 1000 ohms resistance
+        v_1 = input_0.read()            # Voltage between photoresistance and 1000 ohms resistance
         v_2 = 0                         # Ground
 
         # Check the validity of the voltage read
         if v_1 is not None:
-            v_1 *= 5            # Convert 0-1 V to 0-5 V
+            v_1 *= 5            # Convert 0-1 to 0-5 V
             current = (v_1 - v_2) / 1000
             r_photo = (v_0 - v_1) / current
 
@@ -35,6 +35,7 @@ while True:
             print((f"ai_0: {v_0:.3f}, ai_1: {v_1:.3f}, ai_2: {v_2:.3f}, current: {current:.3f},") + 
                    (f"r_photo: {r_photo:.3f}, max_ratio: {max_ratio:.3f}"))
 
+            # Give digital output between min_v_ratio and max_v_ratio
             output.write(max_ratio * (max_v_ratio - min_v_ratio) + min_v_ratio)
         else:
             print(f"\033[1;31;40mVoltage input missing. ai_0: {v_0}, ai_1: {v_1}, ai_2: {v_2}\033[0m")
